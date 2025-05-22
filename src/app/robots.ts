2facb66f02b51eff631095c51ca6948d95790d37
Nayan Shirpure <nayanshirpure@gmail.com>
2025-05-23 01:26:17 +0530

    new robots.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -10,11 +10,13 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: '*',
         allow: '/',
+        disallow: '',
         // Add disallow rules here if needed for specific paths
         // e.g., disallow: ['/admin/', '/tmp/'],
       },
     ],
     sitemap: `${BASE_URL}sitemap.xml`,
+    host: BASE_URL,
     // Optionally, specify a host if your site is accessible via multiple domains
     // host: BASE_URL,
   };
