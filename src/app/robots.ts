72735ea3b61d54b68542addb2ee96604173cbc08
Nayan shirpure <nayanshirpure@gmail.com>
2025-06-12 19:25:56 +0000

    new sitemap

--- src/app/robots.ts
+++
@@ -1,3 +0,0 @@
-// This file should be DELETED.
-// `next-sitemap` is configured to generate your robots.txt.
-// Leaving this file, even if empty or exporting null, might interfere.
--- src/app/sitemap.ts
+++
@@ -1,3 +0,0 @@
-// This file should be DELETED.
-// `next-sitemap` is configured to generate your sitemap.xml.
-// Leaving this file, even if empty or exporting null, might interfere.
